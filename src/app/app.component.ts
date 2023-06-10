0de57bb340a3469de94844b71842e14571aed8af
Mohsen77sk <Mohsen77sk@gmail.com>
2023-06-10 14:01:21 +0330

    add sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { DateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'angular-material-moment-jalali-hijri-adapter';
+  date = new Date();
+
+  /**
+   * constructor
+   */
+  constructor(
+    private _adapter: DateAdapter<any>,
+    @Inject(MAT_DATE_LOCALE) public _locale: any
+  ) {}
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Public methods
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * Change locale to input value
+   *
+   * @param value
+   */
+  changeLocale(value: 'en-US' | 'fa-IR' | 'ar-SA'): void {
+    this._locale = value;
+    this._adapter.setLocale(this._locale);
+
+    this.changeDate(this.date);
+  }
+
+  /**
+   * Check is current locale
+   *
+   * @param value
+   */
+  isLocale(value: string | string[]): boolean {
+    return Array.isArray(value)
+      ? value.some((x) => x === this._locale)
+      : this._locale === value;
+  }
+
+  /**
+   * Change datepicker
+   *
+   * @param value
+   */
+  changeDate(value: Date) {
+    console.log(
+      `Change:
+      Date: ${value}
+      ISO : ${value?.toISOString()}
+      Local(${this._locale}): ${value.toLocaleString(this._locale)}`
+    );
+
+    this.date = value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DynamicMatMomentModule } from './utils/material-adapter';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    DynamicMatMomentModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
